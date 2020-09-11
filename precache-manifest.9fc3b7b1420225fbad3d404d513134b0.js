88a5323e4e1b2ca3be8f31e9d48c8edb2d139962
ozair <truemail785@gmail.com>
2020-09-11 20:42:15 +0500

    Updates

---
+++ precache-manifest.9fc3b7b1420225fbad3d404d513134b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fda93efe92d33b25ce500abc84b3dd1",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "afff26874ad11c8e3505",
+    "url": "/pomodoro-clock/static/css/main.ee2aacf9.chunk.css"
+  },
+  {
+    "revision": "c9cf4c27b7211e12371b",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afff26874ad11c8e3505",
+    "url": "/pomodoro-clock/static/js/main.8b6afa03.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
